5525a911d0608c268bfc4b2f1bffc0416d3dab95
Gonçalo Almeida <Gonalo Almeida@LAPTOP-3G33MCTG>
2020-05-27 16:38:04 +0100

    faz push pls

--- HelloWorld/src/HelloTest.java
+++ HelloWorld/src/HelloTest.java
@@ -1,35 +1,29 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
 
 class HelloTest {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		Hello helloWord;
+		Hello helloworld;
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
+	
 	@Test
-	void testSquare() {
+	void testProduct() {
 		Hello hel = new Hello();
 		int output = hel.product(2, 2);
 		assertEquals(4, output);
 	}
-
 }
